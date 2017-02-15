209d020e2117b7eb3eb39e269f6a577bccc86d54
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-14 22:27:44 -0700

    Erb interpolation in javascript

--- app_express.js
+++ app_express.js
@@ -10,6 +10,7 @@ app.get('/contact', function(request, response){
   response.sendFile(__dirname + '/contact.html')
 });
 app.get('/profile/:name', function(request, response){
-  response.send('You are viewing the profile of ' + request.params.name);
+  var data = {age: 27, job: 'Web Developer'};
+  response.render('profile', {person: request.params.name, data: data });
 });
 app.listen(3000);
