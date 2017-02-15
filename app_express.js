7f05ff3f2509becf2fbe140ffdf1cc5040f1f64f
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-14 22:35:00 -0700

    Learning...

--- app_express.js
+++ app_express.js
@@ -10,7 +10,7 @@ app.get('/contact', function(request, response){
   response.sendFile(__dirname + '/contact.html')
 });
 app.get('/profile/:name', function(request, response){
-  var data = {age: 27, job: 'Web Developer'};
+  var data = {age: 27, job: 'Web Developer', hobbies: ['eating', 'coding', 'kissing']};
   response.render('profile', {person: request.params.name, data: data });
 });
 app.listen(3000);
